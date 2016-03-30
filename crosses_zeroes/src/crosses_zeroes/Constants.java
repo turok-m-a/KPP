cc98bd063788a74dd4d0c918ff94e9c1ffe7b788
turok-m-a <turok.m7@gmail.com>
2016-03-30 21:44:14 +0300

    added save and load (replay) feautures
    Change-Id: I91b049e5b4c8b2432a6df9a8c5e9fa42279cb25e

--- crosses_zeroes/src/crosses_zeroes/Constants.java
+++ crosses_zeroes/src/crosses_zeroes/Constants.java
@@ -4,21 +4,27 @@ package crosses_zeroes;
  *
  */
 public interface Constants {
-  static final int CROSS_MARK = 1;
-  static final int ZERO_MARK = 2;
-  static final int EMPTY_MARK = 0;
-  static final int ZEROS_WIN = 2;   // current game state
-  static final int CROSSES_WIN = 1;
-  static final int DRAW = 0;
-  static final int GAME_IS_NOT_OVER = -1;
-  static final int EASY = 1;        // difficulty
-  static final int MEDIUM = 3;
-  static final int HARD = 5;
-  static final int NO_RESULT = -1;  // if no cell number to return
-  static final int NOTHING_TO_PREVENT = -1;
-  static final int NO_POSSIBILITY = -1;
-  static final int MAX_FIELD_SIZE = 4;
-  static final int STOP_AUTO_GAME = 1;
-  static final int BELONGS_TO_WIN_COMBINATION = 1;
-  static final int DOESNT_BELONG_TO_WIN_COMBINATION = 0;
+  final int CROSS_MARK = 1;
+  final int ZERO_MARK = 2;
+  final int EMPTY_MARK = 0;
+  final int ZEROS_WIN = 2;   // current game state
+  final int CROSSES_WIN = 1;
+  final int DRAW = 0;
+  final int GAME_IS_NOT_OVER = -1;
+  final int EASY = 1;        // difficulty
+  final int MEDIUM = 3;
+  final int HARD = 5;
+  final int NO_RESULT = -1;  // if no cell number to return
+  final int NOTHING_TO_PREVENT = -1;
+  final int NO_POSSIBILITY = -1;
+  final int MAX_FIELD_SIZE = 4;
+  final int STOP_AUTO_GAME = 1;
+  final int BELONGS_TO_WIN_COMBINATION = 1;
+  final int DOESNT_BELONG_TO_WIN_COMBINATION = 0;
+  final int STOP_REPLAY = 1;
+  final int FIRST_TURN = 1;
+  final int THIRD_TURN = 3;
+  final int WINDOW_LENGTH = 450; // window size
+  final int WINDOW_HEIGTH = 130;
+  final int DEFAULT_FIELD_SIZE = 3;
 }
--- crosses_zeroes/src/crosses_zeroes/UserInterface.java
+++ crosses_zeroes/src/crosses_zeroes/UserInterface.java
@@ -1,5 +1,6 @@
 package crosses_zeroes;
 
+import java.io.File;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.Button;
@@ -7,19 +8,23 @@ import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.Pane;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
 /**
  * adds GUI elements to Pane
  */
 public class UserInterface implements Constants {
-  ButtonController buttons;
-  GameLogic field;
-  Pane root;
-
-  UserInterface(ButtonController tempButtons, GameLogic tempField, Pane tempRoot) {
+  private ButtonController buttons;
+  private GameLogic field;
+  private Pane root;
+  File gameReplayFile;
+  Stage mainStage;
+  UserInterface(ButtonController tempButtons, GameLogic tempField, Pane tempRoot, Stage tempStage) {
     buttons = tempButtons;
     field = tempField;
     root = tempRoot;
+    mainStage = tempStage;
   }
 
   void init() {
@@ -120,8 +125,42 @@ public class UserInterface implements Constants {
     autoGameButton.setTranslateY(10);
     autoGameButton.setPrefSize(100, 30);
 
+    //add load and save buttons
+    final FileChooser fileChooser = new FileChooser();
+    //File gameReplayFile;
+    Button loadButton = new Button("Загрузить"); // load replay
+    loadButton.setTranslateX(350);
+    loadButton.setTranslateY(10);
+    loadButton.setPrefSize(100, 30);
+    loadButton.setOnAction(new EventHandler<ActionEvent>() {
+      @Override
+      public void handle(ActionEvent event) {
+        try {
+          gameReplayFile = fileChooser.showOpenDialog(mainStage);
+          buttons.showReplay(gameReplayFile);
+        } catch (NullPointerException e) {
+          System.out.println("file not selected!"); // user clicked "cancel"
+        };
+      }
+    });
+    Button saveButton = new Button("Сохранить"); // save button
+    saveButton.setTranslateX(350);
+    saveButton.setTranslateY(50);
+    saveButton.setPrefSize(100, 30);
+    saveButton.setOnAction(new EventHandler<ActionEvent>() {
+      @Override
+      public void handle(ActionEvent event) {
+        try {
+          gameReplayFile = fileChooser.showSaveDialog(mainStage);
+          field.saveReplay(gameReplayFile);
+        } catch (NullPointerException e) {
+          System.out.println("file not selected!"); // user clicked "cancel"
+        };
+      }
+    });
+
     root.getChildren().addAll(newGameButton, radioThree, radioFour, sizeLabel,
         levelLabel, radioHard, radioMedium);
-    root.getChildren().addAll(autoGameButton, radioEasy);
+    root.getChildren().addAll(autoGameButton, radioEasy, loadButton, saveButton);
   }
 }
